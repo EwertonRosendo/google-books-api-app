08e1501e8fb39653639946dd522e12f43af639e0
ewertonrosendo <ewerton.rosendo15@gmail.com>
2024-08-02 13:04:07 -0300

    bug fix

--- app/services/search_google_api.rb
+++ app/services/search_google_api.rb
@@ -7,27 +7,35 @@ class SearchGoogleApi
   end
 
   def searchBookOnGoogle
-    url = "https://www.googleapis.com/books/v1/volumes?q=title:#{@title}&key=AIzaSyDXXnIr_YKRWAmhO5c0arzwTNj2Dys2h_k"
+    url = "https://www.googleapis.com/books/v1/volumes?q=title:#{@title}&maxResults=30&key=AIzaSyDXXnIr_YKRWAmhO5c0arzwTNj2Dys2h_k"
     uri = URI(url)
 
-    response = JSON.parse(Net::HTTP.get(uri))
-    list_books = []
+    begin
+      response = Net::HTTP.get(uri)
+      parsed_response = JSON.parse(response)
+      list_books = []
 
-    unless response.nil?
-      response["items"].each do |item|
-        volume_info = item["volumeInfo"]
-        book = {
-          "title": volume_info["title"],
-          "subtitle": volume_info["subtitle"],
-          "description": volume_info["description"],
-          "authors": volume_info["authors"],
-          "publishedDate": volume_info["publishedDate"],
-          "publisher": volume_info["publisher"],
-          "thumbnail": volume_info["imageLinks"]["thumbnail"]
-        }
-        list_books.push(book)
+      if parsed_response && parsed_response["items"]
+        parsed_response["items"].each do |item|
+          volume_info = item["volumeInfo"]
+          book = {
+            "title": volume_info["title"],
+            "subtitle": volume_info["subtitle"],
+            "description": volume_info["description"],
+            "authors": volume_info["authors"],
+            "publishedDate": volume_info["publishedDate"],
+            "publisher": volume_info["publisher"],
+            "thumbnail": volume_info["imageLinks"] ? volume_info["imageLinks"]["thumbnail"] : nil
+          }
+          list_books.push(book)
+        end
       end
+
       list_books
+    rescue => e
+      # Trate o erro conforme necessário, por exemplo, registre-o ou mostre uma mensagem de erro
+      puts "Erro ao buscar dados: #{e.message}"
+      []
     end
   end
 end
